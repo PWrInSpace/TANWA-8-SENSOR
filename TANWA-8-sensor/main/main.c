31ae63469ad3eb95fcd7daef76373c090fdb46a4
Miodek233 <jamiodus@gmail.com>
2025-06-18 11:18:46 +0200

    Drivers implementation
    * pierwszy commit ever
    
    * Poprawa bledow
    
    * Fixed files layout
    
    * Fixed files layout
    
    * Fixed file layout
    
    * implementation of basic hardware drivers
    
    * Fixed .gitignore & add led blinking

--- TANWA-8-sensor/main/main.c
+++ TANWA-8-sensor/main/main.c
@@ -2,5 +2,17 @@
 
 void app_main(void)
 {
+    // CONFIGURE THE MESSAGE
 
+    ESP_LOGI(TAG, "%s TANWA board starting", config.board_name);
+    
+    if(setup_task_init() != ESP_OK) {
+        ESP_LOGE(TAG, "Failed to initialize setup task");
+        return;
+    }
+
+    while(1) {
+        led_toggle(&(config.status_led));
+        vTaskDelay(1000 / portTICK_PERIOD_MS);
+    }
 }
